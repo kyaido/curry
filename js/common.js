dae3d138966b3f564f64119ee16cc4619d52ff90
kaido <kaido@team-lab.com>
2014-04-16 20:34:26 +0900

    commit

--- js/common.js
+++ js/common.js
@@ -32,3 +32,43 @@ $(function() {
     
   });
 });
+
+
+
+
+
+
+$(function() {
+  var start = 0;
+  var end = 64;
+  var query = 'ボンディ';
+  var googleApiUrl = 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=large&hl=ja&imgsz=medium';
+  var addTarget = $('.modal .images');
+  var addDom = $('<ul class="clearfix"></ul>');
+  
+  for(var i = start; i < end; i = i + 8 ) {
+    $.ajax({
+      type: 'GET',
+      url: googleApiUrl,
+      dataType: 'jsonp',
+      data: {
+        q: query,
+        start: i
+      },
+      success: function(json) {
+        if (200 === json.responseStatus) {
+          var data = json.responseData.results;
+          for (var i in data) {
+            var img = '<img src="' + data[i].url + '">';
+            $(img).error(function() {
+              img = '<img src="img/blank.gif">';
+            })
+            addDom.append('<li>' + img + '</li>');
+          }
+        }
+      }
+    });
+  }
+  addTarget.append(addDom);
+  
+});
