e510af42d258d957df3d27854cee3031ad4dc4b6
kaido <kaido@team-lab.com>
2014-04-17 18:03:25 +0900

    諸々追加

--- js/common.js
+++ js/common.js
@@ -1,9 +1,67 @@
 
 
+// googlemap
+function initialize(shopLat, shopLng, zoom) {
+  var labLatlng = new google.maps.LatLng(35.705053, 139.756473);
+  var shopLatlng = new google.maps.LatLng(shopLat, shopLng);
+  var myOptions = {
+    zoom: zoom,
+    center: labLatlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById('gmap'), myOptions);
+  
+  //var image = 'img/lab_marker.png';
+  var labMarker = new google.maps.Marker({
+    position: labLatlng,
+    //icon: image,
+    map: map
+  });
+  var shopMarker = new google.maps.Marker({
+    position: shopLatlng,
+    //icon: image,
+    map: map
+  });
+}
+
+
+// google image search
+function imageList(query) {
+  var start = 0;
+  var end = 64;
+  var q = query;
+  var googleApiUrl = 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=large&hl=ja&imgsz=medium';
+  var addTarget = $('.modal .images');
+  var addDom = $('<ul class="clearfix"></ul>');
+  
+  for(var i = start; i < end; i = i + 8 ) {
+    $.ajax({
+      type: 'GET',
+      url: googleApiUrl,
+      dataType: 'jsonp',
+      data: {
+        q: q,
+        start: i
+      },
+      success: function(json) {
+        if (200 === json.responseStatus) {
+          var data = json.responseData.results;
+          for (var i in data) {
+            addDom.append('<li style="background-image:url(' + data[i].url + ');"></li>');
+          }
+        }
+      }
+    });
+  }
+  addTarget.append(addDom);
+}
+
+
 $(function() {
   var point = 0;
   var item = $('.tags li');
   var item_select_count = 0; // max = 3
+  var user_select;
   
   item.on('click', function() {
     if($(this).hasClass('selected')) {
@@ -18,68 +76,34 @@ $(function() {
     }
     
     if(item_select_count === 3) {
-      if(point < 5) {
-        $('.modal').addClass('small');
+      if(point === 5) {
+        user_select = 'five';
+      }
+      else if(point < 5) {
+        user_select = 'small';
       }
       else if(point < 10) {
-        $('.modal').addClass('medium');
+        user_select = 'medium';
       }
       else {
-        $('.modal').addClass('large');
+        user_select = 'large';
       }
+      
+      // create curry shop data
+      var data = CURRY_DATA[user_select];
+      if(data) {
+        $('.modal .introduction span').html(data.introduction);
+        $('.modal .shop').html(data.shop);
+        $('.modal .feature').html(data.feature);
+        // create googlemap
+        initialize(data.lat, data.lng, data.zoom);
+        // create background image list
+        imageList(data.query);
+      }
+      
+      // modal open
       $('body').addClass('modalOpen');
     }
     
   });
 });
-
-
-$(function() {
-  var start = 0;
-  var end = 64;
-  var query = 'ボンディ';
-  var googleApiUrl = 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=large&hl=ja&imgsz=medium';
-  var addTarget = $('.modal .images');
-  var addDom = $('<ul class="clearfix"></ul>');
-  
-  for(var i = start; i < end; i = i + 8 ) {
-    $.ajax({
-      type: 'GET',
-      url: googleApiUrl,
-      dataType: 'jsonp',
-      data: {
-        q: query,
-        start: i
-      },
-      success: function(json) {
-        if (200 === json.responseStatus) {
-          var data = json.responseData.results;
-          for (var i in data) {
-            addDom.append('<li style="background-image:url(' + data[i].url + ');"></li>');
-          }
-        }
-      }
-    });
-  }
-  addTarget.append(addDom);
-  
-});
-
-
-function initialize() {
-  var latlng = new google.maps.LatLng(35.705053, 139.756473);
-  var myOptions = {
-    zoom: 15,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(document.getElementById('gmap'), myOptions);
-  
-  //var image = 'img/lab_marker.png';
-  var labMarker = new google.maps.Marker({
-    position: latlng,
-    //icon: image,
-    map: map
-  });
-}
-initialize();
---
+++ js/data.js
@@ -0,0 +1,49 @@
+
+
+/*
+
+shop = 店名
+introduction = 紹介
+query  = 検索語
+feature  = 特徴
+lat = 緯度
+lng  = 経度
+zoom  = ズーム率
+  
+*/
+var CURRY_DATA = {
+  
+  // small
+  "small": {
+    "shop"         : "small",
+    "introduction" : "small",
+    "query"        : "カレー",
+    "feature"      : "small",
+    "lat"          : "35.69655",
+    "lng"          : "139.757202",
+    "zoom"         : 14
+  },
+  
+  // medium
+  "medium": {
+    "shop"         : "medium",
+    "introduction" : "medium",
+    "query"        : "curry",
+    "feature"      : "medium",
+    "lat"          : "35.699303",
+    "lng"          : "139.757674",
+    "zoom"         : 14
+  },
+  
+  // large
+  "large": {
+    "shop"         : "ハングリー味川",
+    "introduction" : "よくばりなあなたには<br>ハンバーグカレー",
+    "query"        : "ハングリー味川",
+    "feature"      : "ハングリー味川の説明が入りますハングリー味川の説明が入りますハングリー味川の説明が入りますハングリー味川の説明が入りますハングリー味川の説明が入りますハングリー味川の説明が入りますハングリー味川の説明が入りますハングリー味川の説明が入りますハングリー味川の説明が入りますハングリー味川の説明が入ります",
+    "lat"          : "35.701864",
+    "lng"          : "139.753193",
+    "zoom"         : 14
+  },
+  
+};
